6834453b5374e963edfb9d9b643ab0bd44fc1f65
yuanyu90221 <yuanyu90221@gmail.com>
2017-07-13 04:09:24 +0800

    wikit

--- Wikit/eventPage.js
+++ Wikit/eventPage.js
@@ -4,3 +4,24 @@ var menuItem = {
     "contexts": ["selection"]
 };
 chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI(str){
+    return encodeURI(str).replace(/%5B/g,'[').replace(/%5D/g,']');
+}
+// 監聽 右鍵選單
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if( clickData.menuItemId == 'wikit' && clickData.selectionText ){
+        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth / 2,
+            "height": screen.availWidth /2
+        };
+        chrome.windows.create(createData, function(){
+            
+        });
+    }
+});
